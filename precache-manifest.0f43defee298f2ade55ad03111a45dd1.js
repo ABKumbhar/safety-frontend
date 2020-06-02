f99d8584a3e8e103daf13a3cf3fc2f49638d2300
Aniket Kumbhar <akumbhar@ch.iitr.ac.in>
2020-06-02 22:21:32 +0530

    Updates

---
+++ precache-manifest.0f43defee298f2ade55ad03111a45dd1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5e0cdb353b0990f5f8b99415266512e",
+    "url": "/safety-frontend/index.html"
+  },
+  {
+    "revision": "ce479cd46c00d8ca0a56",
+    "url": "/safety-frontend/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "c6a271de0d3ad76e9f2e",
+    "url": "/safety-frontend/static/css/main.41e9e216.chunk.css"
+  },
+  {
+    "revision": "ce479cd46c00d8ca0a56",
+    "url": "/safety-frontend/static/js/2.f19f66d2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/safety-frontend/static/js/2.f19f66d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6a271de0d3ad76e9f2e",
+    "url": "/safety-frontend/static/js/main.4e5a919e.chunk.js"
+  },
+  {
+    "revision": "c278eaef9b1141021abe",
+    "url": "/safety-frontend/static/js/runtime-main.6a72dbbb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/safety-frontend/static/media/logo.5d5d9eef.svg"
+  }
+]);
